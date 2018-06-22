723e0792efb1ef942e615fa959ba355f4410b68d
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-21 21:45:44 -0400

    Add files via upload

--- MMarray.rb
+++ MMarray.rb
@@ -24,5 +24,11 @@ def MinedMinds()
   if i <= 100
     (mmarr[i] % 3 == 0) && (mmarr[i] % 5 == 0)
     mmarr[i] == "Mined Minds"
+  elsif
+    mmarr[i] % 5 == 0
+    mmarr[i] == "minds"
+  elsif
+    mmarr[i] % 3 == 0
+    mmarr[i] == "mined"
   end
 end
