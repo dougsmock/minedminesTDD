72b35fea61e610548bac30ec704853958973be33
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-21 20:54:47 -0400

    Add files via upload

--- MMarray.rb
+++ MMarray.rb
@@ -1,8 +1,81 @@
-
-
 def numbers()
   mmarr = [*1..100]
   mmarr[2] = "mined"
   mmarr[4] = "minds"
   mmarr
 end
+
+def mined()
+  if i <= 100
+    mmarr[i] % 3 == 0
+    mmarr[i] == "mined"
+  end
+  i +=
+end
+
+#i = 1
+#100.times do
+#  puts "#{i}"
+#    i += 1
+#end
+
+
+#p 1
+#puts 1
+#print 1
+
+#100.times do
+# puts 1
+#end
+
+#number = 1
+#100.times do
+#  puts number
+#  number = number + 1
+#end
+
+#replace 3 with "mind"
+
+#number = 1
+#100.times do
+#  if number == 3
+#    puts "mined"
+#  else
+#    puts number
+#  end
+#    number = number + 1
+#end
+
+#first script that works
+
+#number = 1
+#100.times do
+#if number % 15 == 0
+#  puts "Mined Minds"
+#  elsif number % 3 == 0
+#    puts "mined"
+#  elsif number % 5 == 0
+#    puts "minds"
+#  else
+#    puts number
+#  end
+#    number = number + 1
+#end
+
+
+
+#another script that works
+
+#number = 1
+#100.times do
+#  if (number % 3 == 0) && (number % 5 == 0)
+#    puts "Mined Minds"
+#  elsif number % 3 == 0
+#    puts "mined"
+#  elsif number % 5 == 0
+#    puts "minds"
+#  else
+#    puts number
+#  end
+#    number = number + 1
+#end
