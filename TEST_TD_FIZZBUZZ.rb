67ad5370a9e5e04253f081faca5ebd181edbac8e
dougsmock <39506285+dougsmock@users.noreply.github.com>
2018-06-21 20:54:26 -0400

    Add files via upload

--- TEST_TD_FIZZBUZZ.rb
+++ TEST_TD_FIZZBUZZ.rb
@@ -7,13 +7,13 @@ class TestMinedMinds < Minitest::Test
         assert_equal(1, 1)
     end
 
-    def test_assert_array_numbers
-        assert_equal(numbers[0], 1)
-    end
+  #  def test_assert_array_numbers
+  #      assert_equal(numbers[0], 1)
+  #  end
 
-    def test_assert_array_numbers
-        assert_equal(numbers[40], 41)
-    end
+  #  def test_assert_array_numbers
+  #      assert_equal(numbers[40], 41)
+  #  end
 
     def test_assert_position_two_equals_mined
         assert_equal(numbers[2], "mined")
