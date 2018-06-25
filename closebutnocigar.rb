ab8dfe26e1753bc99d4532e234888c0b66d1b1ae
Doug Smock <dougsmock@Dougs-MacBook-Air.local>
2018-06-25 12:46:00 -0400

    finishes code on cbncig

--- closebutnocigar.rb
+++ closebutnocigar.rb
@@ -1,18 +1,11 @@
 def match_check(ticket1, ticket2)
-  1
+  ticket1 = ticket1.split('')
+  ticket2 = ticket2.split('')
+  matchcounter = 0
+  ticket1.each_with_index do |num, index|
+    if num == ticket2[index]
+      matchcounter += 1
+    end
+  end
+  matchcounter
 end
-
-
-
-
-
-
-
-
-
-
-#make function
-#takes 2 string as arguments
-#make sure they match
-#
-#function an array of string, indfinite number
--- test_td_fizzbuzz4.rb
+++ test_td_fizzbuzz4.rb
@@ -7,4 +7,16 @@ class CloseButNoCigar < Minitest::Test
     assert_equal(Integer, match_check("","").class)
   end
 
+  def test_no_match_returns_0
+    assert_equal(0, match_check('5678','1234'))
+  end
+
+  def test_1_match_returns_1
+    assert_equal(1, match_check("1234", "1555"))
+  end
+
+  def test_4_match_returns_4
+    assert_equal(4, match_check("1234", "1234"))
+  end
+
 end
