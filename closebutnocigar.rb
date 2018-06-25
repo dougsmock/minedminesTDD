637d85f968dfafcbd7caf80c5aa31fec0645e0e2
Doug Smock <dougsmock@Dougs-MacBook-Air.local>
2018-06-25 11:03:22 -0400

    failed test

---
+++ closebutnocigar.rb
@@ -0,0 +1,15 @@
+1
+
+
+
+
+
+
+
+
+
+#make function
+#takes 2 string as arguments
+#make sure they match
+#
+#function an array of string, indfinite number
--- test_td_fizzbuzz4.rb
+++ test_td_fizzbuzz4.rb
@@ -1,7 +1,7 @@
 require "minitest/autorun"
 require_relative "closebutnocigar.rb"
 
-class TestMmarray < Minitest::Test
+class CloseButNoCigar < Minitest::Test
 
   def test_match_returns_boolean
     assert_equal(Boolean,match_check("","").class)
