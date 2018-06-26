5bbf949cfa20d9067ef0ef444fed163b7cb920e7
Doug Smock <dougsmock@Dougs-MacBook-Air.local>
2018-06-26 17:39:49 -0400

    Restarting exercise by myself

--- test_td_fizzbuzz4.rb
+++ test_td_fizzbuzz4.rb
@@ -1,35 +1,71 @@
+# require "minitest/autorun"
+# require_relative "closebutnocigar.rb"
+#
+# class CloseButNoCigar < Minitest::Test
+#
+#   def test_match_returns_integer
+#     assert_equal(Integer, match_check("","").class)
+#   end
+#
+#   def test_no_match_returns_0
+#     assert_equal(0, match_check('5678','1234'))
+#   end
+#
+#   def test_1_match_returns_1
+#     assert_equal(1, match_check("1234", "1555"))
+#   end
+#
+#   def test_4_match_returns_4
+#     assert_equal(4, match_check("1234", "1234"))
+#   end
+#
+#   def test_check_list_returns_0_winners
+#     assert_equal(0, check_list(['1234', '2345'], '7777'))
+#   end
+#
+#   def test_if_mytick_equals_winning_tick
+#     assert_equal([4,2,2], checklist(["1234" , "1222", ]))
+#   end
+#
+# end
+
+
 require "minitest/autorun"
 require_relative "closebutnocigar.rb"
 
-class CloseButNoCigar < Minitest::Test
+class TestCigar < Minitest::Test
 
-  def test_match_returns_integer
-    assert_equal(Integer, match_check("","").class)
-  end
+    def test_assert_that_1_equals_1
+        assert_equal(1, 1)
+    end
 
-  def test_no_match_returns_0
-    assert_equal(0, match_check('5678','1234'))
-  end
+    def test_assert_that_numbers_are_strings
+      assert_equal(String, "1234".class)
+    end
 
-  def test_1_match_returns_1
-    assert_equal(1, match_check("1234", "1555"))
-  end
-
-  def test_4_match_returns_4
-    assert_equal(4, match_check("1234", "1234"))
-  end
-
-  def test_check_list_returns_0_winners
-    assert_equal(0, check_list(['1234', '2345'], '7777'))
-  end
-
-  def test_check_list_returns_1_ticket_winner
-    assert_equal(1, check_list(['1234', '2345'], '1234'))
-  end
-
-  def test_if_moretickets_equals_winningticket
-    assert_equal([4, 3], moretickets(["1233", "1234"], "1234"))
-  end
-
+    # def test_assert_that_strings_have_4_digits
+    #  assert_equal
+    # end
 
 end
+
+
+
+
+
+    #
+    # def test_assert_tickets_compared
+    #   assert_equal
+    # end
+    #
+    # def test_assert_zero_matched
+    #   assert_equal
+    # end
+    #
+    # def test_assert_if_all_4_matched
+    #   assert_equal
+    # end
+    #
+    # def test_assert_if_only_1_missed
+    #   assert_equal
+    # end
--- test_tdfizz2.rb
+++ test_tdfizz2.rb
@@ -17,3 +17,26 @@ class Calculator < Minitest::Test
 
 
 end
+
+
+
+
+    # def test_if_function_has_string
+    # 	assert_equal(Integer, cigar("","").class)
+    # end
+    #
+    # def test_if_off_by_3
+    # 	assert_equal(3, cigar("1234", "1555"))
+    # end
+    #
+    # def test_if_off_by_1
+    # 	assert_equal(1,cigar("1235","1245"))
+    # end
+    #
+    # def test_if_mytick_off_by
+    #   assert_equal([0,2,2], cigararr(["1234", "1222", "1245"], "1234"))
+    # end
+    #
+    # def test_if_winners_equal_2
+    #     assert_equal([2,1], winarr(["1234","1234","1247","1255"], ["1234", "1247"]))
+    # end
