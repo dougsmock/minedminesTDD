1632bddf571d476c2bad9d6166cc570a287582fb
Doug Smock <dougsmock@Dougs-MacBook-Air.local>
2018-06-27 09:56:29 -0400

    added a test

--- closebutnocigar.rb
+++ closebutnocigar.rb
@@ -1,4 +1,35 @@
-digitsarr = []
-winner = "1234"
-digitsarr = winner.split('')
-digitsarr.push(winner)
+def cigar(num,win_num)
+	num = num.split("")
+	win_num = win_num.split("")
+	counter = 0
+	num.each_with_index do |ticket, index|
+		if ticket != win_num[index]
+			counter += 1
+		end
+	end
+	counter
+end
+
+def cigararr(mytickarray, winning_tick)
+	ret = []
+	mytickarray.each do |ticket|
+		ret << cigar(ticket, winning_tick)
+	end
+	ret
+end
+
+def winarr(mytickarray, winning_tick)
+	warr = []
+
+	winning_tick.each do |ticket|
+	matches = cigararr(mytickarray, ticket)
+	winners = 0
+		matches.each_with_index do |m|
+			if m == 0
+			winners += 1
+			end
+		end
+		warr << winners
+	end
+	warr
+end
