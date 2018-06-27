e382d7a16ccae09940746071c773a919b1fb3b25
Doug Smock <dougsmock@Dougs-MacBook-Air.local>
2018-06-26 20:43:13 -0400

    Frustrating failure on array.

--- closebutnocigar.rb
+++ closebutnocigar.rb
@@ -1,8 +1,2 @@
-def draw_number(digits, winner)
-  digits[] = winner.split('')
-  # puts digits[0], digits[1], digits[2], digits[3]
-end
-
-digits = []
 winner = "1234"
-draw_number(winner)
+digits = winner.split('')
