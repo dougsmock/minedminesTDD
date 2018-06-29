0a622e2806d172b4ee95a30e7752ca53b78e8b56
Doug Smock <dougsmock@Dougs-MacBook-Air.local>
2018-06-29 14:54:21 -0400

    still at one failure

--- lottery2.rb
+++ lottery2.rb
@@ -2,37 +2,39 @@ def draw_numbers()
   numbers = []
     (0..3).each do |drawing|
       drawing = rand(10).to_s
-      numbers << (drawing)
+      numbers << drawing
     end
     official_result = numbers[0] + numbers[1] + numbers[2] + numbers[3]
     official_result
 end
 
+# should be good.
+# def buy_tickets()
+#   random_ticket = []
+#   (0.999).each do
+#     digits = []
+#     (0..3).each do |digits|
+#       i = 0
+#       digits[i] = rand(10).to_s
+#       i += 1
+#     end
+#     guesses = []
+#     guesses << digits[0] + digits[1] + digits[2] + digits[3]
+#     random_ticket << guesses
+#   end
+#   random_ticket
+# end
 
-
-def array_buy_tickets()
-  random_ticket = []
-  (0.999).each do
-    digits = []
-    (0..3).each do |digits|
-      i = 0
-      digits[i] = rand(10).to_s
-      i += 1
-    end
-    guesses = []
-    guesses << digits[0] + digits[1] + digits[2] + digits[3]
-    random_ticket << guesses
-  end
-  random_ticket
-end
-
-
-#introduce guesses array
+# IN LONG LOOP
 # introduce random-ticket array.
+# introduce digits array which will ...
+# ... be cleaned out with each iteration
+
+# IN SHORT LOOP
 # generate first digit
 # generate second digit
 # generate third digit
 # generate fourth digit
 # concatenate digits
 # shovel 4-digit number into random_ticket array.
-# clean out digit array do another drawing.
+# clean out digits array, do another drawing.
--- testfizz_lottery.rb
+++ testfizz_lottery.rb
@@ -3,22 +3,24 @@ require_relative "lottery2.rb"
 
 class Lottery2 < Minitest::Test
 
-  # def test_assert_that_1_equals_1
-  #   assert_equal(1, 1)
-  # end
+  def test_assert_that_1_equals_1
+    assert_equal(1, 1)
+  end
 
-  # def test_if_function_has_string
-  #   assert_equal(String, ("").class)
-  # end
+  def test_if_function_has_string
+    assert_equal(String, ("").class)
+  end
 
   def test_zero_equals_1_character
     numbers = []
     assert_equal(numbers[0], draw_numbers())
   end
 
-  # def test_official_result_4_digit_string
-  #   assert_equal(official_result, "")
-  # end
+  def test_official_result_4_digit_string
+    official_result = []
+    assert_equal([], official_result, draw_numbers())
+  end
+
   #
   # def test_drawing_is_single_digit_string
   #   assert_equal(drawing, "1")
